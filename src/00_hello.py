f5809d45a0cfc543283e8cd73c2f1a826fc7314e
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-09-02 22:06:40 -0400

    added notes to 00 file

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,2 +1,13 @@
 # Print "Hello, world!" to your terminal
 print('Hello, world!')
+
+
+
+# Playing around and adding others
+
+print('Learning python')
+# You can print anything above is a string
+
+
+print(5705207878)
+# Above is a number or integer
