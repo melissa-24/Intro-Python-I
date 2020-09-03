44c6f77526b8bc2027f8de1520e911ef52fa60db
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-09-02 22:04:06 -0400

    added some notes

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,12 +10,18 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 print("This is the name of the program:", sys.argv[0])
+# This prints the file name
+
 
 # Print out the OS platform you're using:
 print("The platform I am using is:", sys.platform)
+# This prints win32 for me as it is a windows pc
+
+
 
 # Print out the version of Python you're using:
 print('The python version I have is:', sys.version)
+# This prints 3.8.5 as that is the pythong version installed
 
 
 import os
@@ -24,11 +30,16 @@ import os
 # Print the current process ID
 pid = os.getpid()
 print(pid)
+# This prints the current process running
+
 
 # Print the current working directory (cwd):
 path = os.getcwd()
 print(path)
+# This will print the current file location or path
+
 
 # Print out your machine's login name
 name = os.getlogin()
 print(name)
+# On this pc it will print irish as that is the username of this pc
