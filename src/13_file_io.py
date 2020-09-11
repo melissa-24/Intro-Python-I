9181f3999529dc1f46e8934410b85f2d7ecd3ecd
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-09-10 21:20:09 -0400

    Ok that was easy 13 is done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open("foo.txt") as f:
+    print(f.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +19,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar = open("bar.txt", "w+")
+bar.write("Hello, World!\n")
+bar.write("Will this work?\n")
+bar.write("By golly Melissa I think you got it\n")
+bar.close()
